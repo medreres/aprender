640561954223186c6dba9c6827fe9997bd4865b5
medreres <90519417+medreres@users.noreply.github.com>
2022-11-21 22:45:31 +0200

    static files root

--- project/settings.py
+++ project/settings.py
@@ -138,6 +138,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
 )
